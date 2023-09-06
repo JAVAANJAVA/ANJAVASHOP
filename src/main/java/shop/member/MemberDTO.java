090842fba37bc6f97cc2c3cc5e6a6bdd1631f0ed
GENIE KR <39715747+KissGenie@users.noreply.github.com>
2023-09-06 23:36:12 +0900

    테이블별 DTO입니다

---
+++ src/main/java/shop/member/MemberDTO.java
@@ -0,0 +1,46 @@
+package shop.member;
+
+public class MemberDTO {
+	private int idx;//고유번호
+	private String username; // 회원 아이디
+    private String pass; // 비밀번호
+    private String zipcode; // 우편번호
+    private String address; // 주소
+    private String de_address; // 상세주소
+    public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDe_address() {
+		return de_address;
+	}
+	public void setDe_address(String de_address) {
+		this.de_address = de_address;
+	}
+}
